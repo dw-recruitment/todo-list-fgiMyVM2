21f99045483f45b4173ce7c6b20701e0da540f3d
Anonymous Candidate <anon@example.com>
2016-03-21 18:33:42 -0400

    Complete step 2

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "TODO Application for Democracy Works"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.0"]
+                 [ring "1.4.0"]]
   :main ^:skip-aot todoapp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/todoapp/core.clj
+++ src/todoapp/core.clj
@@ -1,7 +1,25 @@
 (ns todoapp.core
+  (:require [clojure.java.io :as io]
+            [compojure.core :refer [defroutes GET]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.content-type :refer [wrap-content-type]]
+            [ring.middleware.resource :refer [wrap-resource]])
   (:gen-class))
 
+(defroutes handler
+           (GET "/" [] (slurp (io/resource "index.html"))))
+
+(def app
+  (-> handler
+      (wrap-resource "public")
+      (wrap-content-type)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Starting server. Press ctrl+c to quit.")
+  (run-jetty #'app {:port 3000 :join? true}))
+
+(comment
+  ;; Run in repl for development
+  (defonce server (run-jetty #'app {:port 3000 :join? false}))
+  )
