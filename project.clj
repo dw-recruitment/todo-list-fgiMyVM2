f1e2a178779a849cd27fcda0eac784b2b3a19af5
Anonymous Candidate <anon@example.com>
2016-03-22 17:33:11 -0400

    Add Hiccup

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.0"]
                  [com.datomic/datomic-free "0.9.5350"]
+                 [hiccup "1.0.5"]
                  [ring "1.4.0"]]
   :main ^:skip-aot todoapp.web
   :target-path "target/%s"
--- src/todoapp/web.clj
+++ src/todoapp/web.clj
@@ -1,14 +1,42 @@
 (ns todoapp.web
-  (:require [clojure.java.io :as io]
-            [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [defroutes GET]]
+            [hiccup.page :refer [html5]]
+            [hiccup.element :refer [image]]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.content-type :refer [wrap-content-type]]
             [ring.middleware.resource :refer [wrap-resource]])
   (:gen-class))
 
+(defn home-page
+  []
+  (html5
+    [:head [:title "TODO List Manager"]]
+    [:body
+     [:h1 "TODO List Manager"]
+     (image
+       {:width "574" :height "51"}
+       "/images/under-construction.gif"
+       "Under Construction")]))
+
+(defn about-page
+  []
+  (html5
+    [:head [:title "About the TODO List Manager"]]
+    [:body
+     [:h1 "About the TODO List Manager"]
+     [:p "You can use this application to manage a list of things you need to get done."]
+     [:h2 "Planned Features:"]
+     [:ul
+      [:li "Displaying items"]
+      [:li "Saving and restoring items"]
+      [:li "Adding new items to the list"]
+      [:li "Marking items as complete"]
+      [:li "Deleting items"]
+      [:li "Multiple lists"]]]))
+
 (defroutes handler
-           (GET "/" [] (slurp (io/resource "index.html")))
-           (GET "/about" [] (slurp (io/resource "about.html"))))
+           (GET "/" [] (home-page))
+           (GET "/about" [] (about-page)))
 
 (def app
   (-> handler
