988936dc8aa3f8c05162cf590185d2884e61fd3a
Anonymous Candidate <anon@example.com>
2016-03-23 12:51:11 -0400

    Delete database after web tests

--- test/todoapp/web_test.clj
+++ test/todoapp/web_test.clj
@@ -3,15 +3,25 @@
             [clj-http.client :as client]
             [clojure.string :as string]
             [clojure.test :refer :all]
+            [datomic.api :as d]
             [todoapp.app :as app]
             [todoapp.web :as web]))
+
+(def uri "datomic:mem://todos-test")
+
+(defn test-fixture
+  [f]
+  (f)
+  (d/delete-database uri))
+
+(use-fixtures :each test-fixture)
 
 (deftest smoke-test
   (testing "Jetty starts and serves home page"
     (let [system (component/start
                    (app/todo-system
                      {:web-server-port 0
-                      :database-uri    "datomic:mem://todos-test"}))
+                      :database-uri    uri}))
           port (web/get-bound-port (:web-server system))
           response (client/get (str "http://localhost:" port))]
       (is (= 200 (:status response)))
@@ -23,7 +33,7 @@
           system (component/start
                    (app/todo-system
                      {:web-server-port 0
-                      :database-uri    "datomic:mem://todos-test"}))
+                      :database-uri    uri}))
           port (web/get-bound-port (:web-server system))
           response (client/post (str "http://localhost:" port)
                                 {:form-params {:new-item-text new-item-text}})]
