b570f0e915cefbe0465cdf077139faf2804fca44
Anonymous Candidate <anon@example.com>
2016-03-23 12:23:51 -0400

    Add form for adding new items

--- src/todoapp/web.clj
+++ src/todoapp/web.clj
@@ -1,22 +1,25 @@
 (ns todoapp.web
   (:require [com.stuartsierra.component :as component]
-            [compojure.core :refer [routes GET]]
+            [compojure.core :refer [routes GET POST]]
             [datomic.api :as d :refer [db q]]
             [hiccup.page :refer [html5]]
             [hiccup.form :as form]
+            [hiccup.util :refer [escape-html]]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.content-type :refer [wrap-content-type]]
+            [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.resource :refer [wrap-resource]]
+            [ring.util.response :as response]
             [todoapp.database :as todo-db]))
 
-(defn todo-map->hiccup
+(defn item-map->hiccup
   [{:keys [id text status]}]
   [:tr
    [:td (form/check-box
           {:disabled true}
           (str "checkbox-" id)
           (= :done status))]
-   [:td text]])
+   [:td (escape-html text)]])
 
 (defn home-page
   [database]
@@ -25,8 +28,24 @@
     [:body
      [:h1 "TODO List Manager"]
      [:table
-      (map todo-map->hiccup
-           (todo-db/get-items (db (:conn database))))]]))
+      (map item-map->hiccup
+           (todo-db/get-items (db (:conn database))))
+      [:tr
+       [:td]
+       [:td (form/form-to [:post "/"]
+                          [:input {:name "new-item-text"}]
+                          " "
+                          [:button {:type "submit"}
+                           "Add New Item"])]]]]))
+
+(defn post-new-item
+  [database item-text]
+  @(todo-db/add-item database {:text item-text})
+  ;; Redirects so user can refresh the page without resending POST data.
+  ;; If this app is ever deployed on multiple load-balanced servers,
+  ;; some coordination might be needed to make sure client sees the most
+  ;; recent items.
+  (response/redirect "/" :see-other))
 
 (defn about-page
   []
@@ -52,10 +71,12 @@
   DatomicDatabase component `database`."
   [database]
   (-> (routes (GET "/" [] (home-page database))
+              (POST "/" [new-item-text] (post-new-item database new-item-text))
               (GET "/about" [] (about-page))
               (GET "/test" [] "hello"))
       (wrap-resource "public")
-      (wrap-content-type)))
+      (wrap-content-type)
+      (wrap-params)))
 
 (defn make-dynamic-handler
   "Calls make-handler on every request so new routes
--- test/todoapp/web_test.clj
+++ test/todoapp/web_test.clj
@@ -1,6 +1,7 @@
 (ns todoapp.web-test
   (:require [com.stuartsierra.component :as component]
             [clj-http.client :as client]
+            [clojure.string :as string]
             [clojure.test :refer :all]
             [todoapp.app :as app]
             [todoapp.web :as web]))
@@ -14,6 +15,20 @@
           port (web/get-bound-port (:web-server system))
           response (client/get (str "http://localhost:" port))]
       (is (= 200 (:status response)))
+      (component/stop system))))
+
+(deftest new-todo-test
+  (testing "New item shows up after adding"
+    (let [new-item-text "Remember the milk"
+          system (component/start
+                   (app/todo-system
+                     {:web-server-port 0
+                      :database-uri    "datomic:mem://todos-test"}))
+          port (web/get-bound-port (:web-server system))
+          response (client/post (str "http://localhost:" port)
+                                {:form-params {:new-item-text new-item-text}})]
+      (is (= 200 (:status response)))
+      (is (string/includes? (:body response) new-item-text))
       (component/stop system))))
 
 (comment
